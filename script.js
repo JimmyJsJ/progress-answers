8cf3379cccfee6174e4c398b43081fa042bf2456
JimmyJsJ <112990767+JimmyJsJ@users.noreply.github.com>
2025-10-03 22:29:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
     questions.forEach((q, i) => {
         let opts = q.querySelectorAll('input[type="radio"]:not(:checked), [class*="option"] input');
         if (opts.length) {
-            opts[0].click();
-            console.log('Answered Q' + i + ': ' + (opts[0].nextSibling?.textContent || opts[0].value || 'Option'));
+            let bestOpt = opts[0]; // Default to first
+            let maxLength = 0;
+            opts.forEach(opt => {
+                let text = opt.nextSibling?.textContent || opt.value || '';
+                if (text.length > maxLength || text.toLowerCase().includes('correct')) {
+                    maxLength = text.length;
+                    bestOpt = opt;
+                }
+            });
+            bestOpt.click();
+            console.log('Answered Q' + i + ': ' + (bestOpt.nextSibling?.textContent || bestOpt.value));
         }
     });
-    alert('Finished answering!');
+    alert('Done answering!');
 })();
